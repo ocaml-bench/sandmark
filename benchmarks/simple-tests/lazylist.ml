ec7a30e4a032c7ff1ee2d04e4b53995567cf7261
Sadiq Jaffer <sadiq@toao.com>
2019-03-10 17:46:02 +0000

    add lazy test

---
+++ benchmarks/simple-tests/lazylist.ml
@@ -0,0 +1,19 @@
+let list_length = int_of_string Sys.argv.(1)
+let iterations = int_of_string Sys.argv.(2)
+
+let rec add_lazy l n =
+    if n == 0 then
+        l
+    else
+        let new_head = lazy (1 + (Lazy.force (List.hd l))) in
+            new_head :: add_lazy l (n-1)
+
+let create_list () =
+    add_lazy [Lazy.from_val 0] list_length
+
+let () = 
+    for _ = 0 to iterations do
+        let l = create_list() in
+            let list_head = List.hd l in
+                ignore(Lazy.force list_head)
+    done
