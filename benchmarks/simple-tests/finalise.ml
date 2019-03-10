9d35f09cb9b734eb6db911a7e5f356ed9710d23a
Sadiq <sadiq@toao.com>
2019-03-10 20:26:47 +0000

    add a test for finalisers

---
+++ benchmarks/simple-tests/finalise.ml
@@ -0,0 +1,17 @@
+let percent_finalize = int_of_string Sys.argv.(1)
+let words_to_allocate = 1000000000.
+
+type a_record = { an_int: int; mutable a_string : string; a_float : float }
+
+let allocate () =
+  for y = 0 to (if percent_finalize > 0 then percent_finalize*10 else 1000) do
+    let v = { an_int = 5; a_string = "foo"; a_float = 0.0 } in
+    if percent_finalize > 0 && y mod percent_finalize == 0 then
+      Gc.finalise (fun n -> ignore(n.an_int+1)) v
+    else
+      ignore(ref v)
+  done
+
+let () = while (Gc.minor_words() < words_to_allocate) do
+    allocate()
+  done
