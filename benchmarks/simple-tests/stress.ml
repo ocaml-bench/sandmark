e9b9c1835320c96ee41d7f5af26c0f604d4181dc
Sadiq <sadiq@toao.com>
2019-03-20 12:37:07 +0000

    just use random for this

--- benchmarks/simple-tests/stress.ml
+++ benchmarks/simple-tests/stress.ml
@@ -1,8 +1,8 @@
-open Printf
-
 let list_length = int_of_string Sys.argv.(1)
-let percent_retain = 100 / int_of_string Sys.argv.(2)
+let percent_retain = int_of_string Sys.argv.(2)
 let iterations = 100000
+
+let () = Random.init 42
 
 let rec init_list acc i n =
   if i >= n then acc
@@ -11,9 +11,9 @@ let rec init_list acc i n =
 let retain_list = ref (init_list [] 0 list_length)
 
 let allocate () =
-  for y = 0 to 1000 do
+  for _ = 0 to 1000 do
     let v = (0,0) in
-    if y mod percent_retain == 0 then
+    if Random.int 100 < percent_retain then
       retain_list := (ref v) :: (List.tl !retain_list)
   else
     ignore(Sys.opaque_identity ref v)
--- benchmarks/simple-tests/weakretain.ml
+++ benchmarks/simple-tests/weakretain.ml
@@ -1,15 +1,17 @@
 type a_mutable_record = { mutable an_int : int; a_string : string ; a_float: float }
 
-let weak_percent = 100 / int_of_string Sys.argv.(1)
+let weak_percent = int_of_string Sys.argv.(1)
 let weak_array_size = int_of_string Sys.argv.(2)
+
+let () = Random.init 42
 
 let weak_array = Weak.create weak_array_size
 
 let () = 
   let v = ref 0 in
   for i = 1 to 100_000_000 do
-    let t = { an_int = 0; a_string = "foo"; a_float = 0.0 } in
-    if i mod weak_percent == 0 then
+    let t = { an_int = i; a_string = "foo"; a_float = 0.0 } in
+    if Random.int 100 < weak_percent then
       begin
         Weak.set weak_array !v (Some t);
         v := (!v + 1) mod weak_array_size
