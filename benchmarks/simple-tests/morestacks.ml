d188361b2f7d681cfb8dca9a582afe8f4a50532e
Tom Kelly <ctk21@cl.cam.ac.uk>
2019-05-14 17:17:41 +0100

    Adjust benchmark parameters to make things run for longer than 10ms and also try to squeeze some of the longer tests that are simple functions in a loop

--- benchmarks/simple-tests/morestacks.ml
+++ benchmarks/simple-tests/morestacks.ml
@@ -5,6 +5,6 @@ let foo () = for a = 0 to 1_000_000 do
   ignore(Sys.opaque_identity bar a)
 done
 
-let () = for _ = 0 to 10_000 do
+let () = for _ = 0 to (try int_of_string(Array.get Sys.argv 1) with _ -> 10_000) do
   foo()
 done
