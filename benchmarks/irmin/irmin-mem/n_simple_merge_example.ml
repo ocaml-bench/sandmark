3d57e8243219ba8b99d997fdf30d2fd608aae1a7
shubhamkumar13 <kumar.shubham1312@gmail.com>
2020-01-24 15:53:31 +0530

    Add an example which can be used for benchmarking irmin.

---
+++ benchmarks/irmin/irmin-mem/n_simple_merge_example.ml
@@ -0,0 +1,101 @@
+let log_one =
+   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id lacus at quam rutrum gravida. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec nisi ipsum, ultricies non enim id, facilisis iaculis magna. In et nisi volutpat, ultricies justo a, consectetur lorem. Integer odio lacus, suscipit ut nisi eu, vehicula commodo massa. Praesent rutrum dignissim est, nec sodales tellus elementum et. Etiam id nulla et lectus blandit dapibus. Cras sed mollis quam. Pellentesque gravida varius dolor, vulputate commodo metus condimentum id. Suspendisse placerat turpis eu pretium blandit. Aliquam eu purus sit amet mauris convallis pulvinar. Aliquam egestas augue ac lorem facilisis, consequat sodales erat pulvinar. Quisque sollicitudin placerat massa, ut aliquam eros porttitor quis.Nam posuere, risus molestie volutpat lacinia, libero nibh fringilla sem, sit amet suscipit ligula erat in nisl. Aenean pharetra libero et ex consectetur varius. Cras vulputate accumsan neque non dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas imperdiet, ex vel vehicula elementum, enim urna semper augue, quis auctor turpis magna id nunc. Nulla auctor tellus nec viverra luctus. Phasellus ut magna sed justo fermentum cursus. Sed bibendum erat nisi, vitae ornare justo gravida eu. Nulla facilisi. Quisque ac dapibus lorem, eget volutpat dui. Phasellus laoreet non lectus eu aliquet. Integer elementum, tellus in tincidunt placerat, justo mi convallis augue, at porttitor nunc tortor eu enim. Maecenas laoreet tortor at lacus feugiat, a ullamcorper ipsum varius. Etiam non diam ante.Duis purus enim, viverra a eleifend id, efficitur eget nunc. Suspendisse sed nibh tincidunt, elementum tortor a, consequat urna. Curabitur lacinia porta nulla, quis porta nunc vehicula in. Aliquam nec risus nisl. Nunc ullamcorper semper dignissim. Aliquam suscipit sapien et metus bibendum imperdiet. Mauris id metus libero. Nullam neque ipsum, tempus vitae mauris id, sollicitudin venenatis tortor. Vivamus at porttitor quam. Nulla ac ultricies metus. Integer in urna enim. Ut imperdiet odio sagittis sapien facilisis dictum. Nullam ultricies neque metus, vitae porttitor neque lobortis ac. Mauris quam magna, gravida a faucibus posuere, pellentesque sed eros. Quisque pharetra orci ac auctor euismod. Proin facilisis quam eu ipsum suscipit, ac dictum sem fermentum.Ut augue arcu, blandit nec nunc at, dictum porttitor massa. Pellentesque nec lorem massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque eget nunc rhoncus mi dignissim faucibus. Curabitur sit amet mi a leo cursus commodo. Donec sed accumsan sapien, eget ullamcorper dolor. Duis ut ultrices magna, eu finibus metus. Vivamus lobortis, augue sed luctus fringilla, tortor orci elementum orci, et viverra nunc tortor eget lectus. Nunc sagittis fermentum rhoncus. Proin placerat, eros sed placerat consequat, enim quam cursus urna, sit amet volutpat lorem velit eu sapien. Praesent et nulla a ligula sollicitudin convallis.Sed vulputate, lorem vitae sodales varius, metus justo gravida ligula, eu semper felis urna ac ante. Pellentesque nec bibendum enim, sit amet pretium sem. Curabitur vel ullamcorper diam. Etiam a nibh in sem tincidunt vestibulum in consectetur tellus. Morbi id augue id sapien pretium aliquam. Proin vel porttitor sem. Phasellus porta turpis id risus posuere, et dignissim augue ultrices. Vestibulum fringilla purus quis turpis elementum, ac malesuada diam gravida."
+
+let log_two = 
+    " Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum consequat facilisis ante congue porttitor. Vestibulum nisl ipsum, imperdiet a urna vel, iaculis condimentum lacus. In at odio ultricies, dictum ipsum in, tincidunt mi. Etiam pharetra est libero, eu ornare tortor elementum ut. Sed et neque id felis auctor aliquam. Cras feugiat lacinia semper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas at nisl tincidunt, lobortis leo id, tempor tortor. Aliquam vel finibus orci. Cras eget nibh at metus luctus ullamcorper. Quisque ornare dolor quis pretium fringilla. Vivamus sed velit sit amet dolor malesuada tincidunt id a nisl. Donec fermentum arcu ac consectetur fringilla. Nulla blandit iaculis congue. Pellentesque in congue metus.
+
+    Proin porttitor non mauris vitae dictum. Donec id purus in magna rhoncus pellentesque tincidunt eget ipsum. Nunc accumsan blandit felis sed pulvinar. Sed varius interdum sodales. Duis felis leo, fringilla sed sollicitudin dapibus, faucibus quis nisi. Morbi at odio felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin id sem consequat dui mollis pretium ut rutrum arcu. Vivamus condimentum eget odio ac blandit. Nullam lacinia dictum leo quis mattis. Aliquam erat volutpat. Donec ut mi velit. Curabitur consequat ex eros, quis vulputate metus luctus vitae.
+
+    Aliquam iaculis nulla lectus, non blandit metus suscipit vel. Praesent odio metus, rhoncus sit amet tortor at, interdum iaculis augue. Integer sed aliquet mauris, sit amet vehicula lectus. Donec suscipit nisi erat, non efficitur sapien auctor vitae. Integer et dapibus sapien. Donec faucibus sollicitudin posuere. Nullam varius augue ipsum, id lacinia augue egestas vel. Morbi vulputate lectus felis, non vulputate ligula viverra sit amet.
+
+    Fusce dapibus ac ante quis cursus. Nam non magna et mi tincidunt rutrum eu et elit. Nulla magna leo, lobortis a dapibus eget, malesuada dapibus quam. Ut mollis nisi vel facilisis pretium. Sed dignissim tellus tellus, eu aliquam purus dictum quis. Sed venenatis libero et risus commodo elementum. Maecenas interdum tincidunt ipsum pellentesque facilisis. Curabitur at nisi nisi. Curabitur egestas quis purus eu efficitur. Vestibulum porttitor leo eleifend, condimentum eros vel, iaculis turpis. Morbi gravida justo a sapien gravida, sed maximus odio elementum. Donec pellentesque blandit nulla.
+
+    Donec vehicula nunc diam, quis convallis arcu ultrices a. Nam fringilla, magna eu ornare rutrum, tellus ligula hendrerit nisi, vitae dictum eros leo eget lacus. Donec et varius mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras consectetur mollis metus vitae scelerisque. Morbi eget porta neque. Sed in odio malesuada, posuere turpis quis, vehicula urna. Ut tincidunt nunc accumsan posuere gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce semper, quam in vehicula vehicula, lectus ante condimentum libero, at blandit lorem odio sed lectus. Aenean at ante enim. Donec posuere, risus sed luctus vehicula, diam libero sodales magna, nec tristique justo elit tincidunt erat. Vivamus lacinia tellus eleifend augue consectetur tempor. Aliquam erat volutpat. Maecenas id tincidunt sem. Cras quis ex egestas augue fermentum fringilla. 
+    Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque nec risus elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum sapien quis eleifend pharetra. Suspendisse iaculis eu sapien id tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus semper ligula a sollicitudin luctus."
+
+(* Simple example of reading and writing in a Git repository *)
+open Lwt.Infix
+open Printf
+
+module Config = struct
+let root = "/tmp/irmin/test"
+
+let init () =
+  let _ = Sys.command (Printf.sprintf "rm -rf %s" root) in
+  let _ = Sys.command (Printf.sprintf "mkdir -p %s" root) in
+  ()
+
+(* Install the FS listener. *)
+let () = Irmin_unix.set_listen_dir_hook ()
+end
+
+let info = Irmin_unix.info
+
+
+module Store = Irmin_unix.Git.FS.KV (Irmin.Contents.String)
+
+let update t k v =
+  let msg = sprintf "Updating /%s" (String.concat "/" k) in
+  print_endline msg;
+  Store.set_exn t ~info:(info "%s" msg) k v (* t : string -> k : string list -> v : string -> unit Lwt.t *)
+
+let read_exn t k =
+  let msg = sprintf "Reading /%s" (String.concat "/" k) in
+  print_endline msg;
+  Store.get t k (* t: string -> k : string list -> contents Lwt.t *)
+
+let para_to_list paragraph = paragraph
+  |> String.split_on_char '.' 
+  |> List.map String.trim
+
+
+let format (id : int) : string = sprintf "%s.txt" (string_of_int id)
+
+(** mutation of new cloned branch
+    includes 
+      - cloning
+      - update with value
+      - merge into the master branch *)
+
+let custom_clone src dst content key_list =
+  let counter = ref 0 in  
+  let get_name x = counter := !counter + 1; (string_of_int counter.contents) ^ x in
+  let change_last (x : string list) =
+    let rec init x = match x with [] -> [] | _ :: [] -> [] | hd :: tl -> hd :: (init tl) in
+    let last = List.rev x |> List.hd |> (^) (string_of_int !counter ^ "_") in
+    let init_list = init x in
+    List.append init_list [last]
+  in
+  let rec custom_clone' src (dst : string) (content : string list) (key_list : string list) =
+    match content with
+    | []       -> Lwt.return_unit
+    | hd :: tl -> Store.clone ~src:src ~dst:dst >>= fun x ->
+                  update x (change_last key_list) hd >>= fun () ->
+                  Store.merge_into ~info:(info "t: Merge with 'x'") x ~into:src >>= function
+                  | Error _ -> failwith "conflict"
+                  | Ok ()   -> print_endline "merging ...";
+                  custom_clone' src (get_name dst) tl key_list
+    (* we generate our own dst and key_list 
+      get_name produces the dst branch
+      change_last genrates the new text file name *)
+  in
+  custom_clone' src dst (para_to_list content) key_list >>= fun () -> Lwt.return_unit
+
+let main () =
+  Config.init ();
+  let config = Irmin_git.config ~bare:true Config.root in
+  Store.Repo.v config >>= fun repo ->
+  Store.master repo >>= fun t -> 
+  update t ["root"; "misc"; "1.txt"] log_one >>= fun () ->
+  custom_clone t "test" log_two ["root"; "misc" ; "sample.txt"] >>= fun () ->
+  read_exn t ["root"; "misc"; "72_sample.txt"] >>= fun _ -> Lwt.return_unit
+  
+let () =
+  Printf.printf
+    "This example creates a Git repository in %s and use it to read \n\
+     and write data:\n"
+    Config.root;
+  let _ = Sys.command (Printf.sprintf "rm -rf %s" Config.root) in
+  Lwt_main.run (main ());
+  Printf.printf "You can now run `cd %s && tig` to inspect the store.\n"
+    Config.root
---
+++ benchmarks/irmin/irmin-mem/simple_merge_example.ml
@@ -0,0 +1,59 @@
+(* Simple example of reading and writing in a Git repository *)
+open Lwt.Infix
+open Printf
+
+module Config = struct
+let root = "/tmp/irmin/test"
+
+let init () =
+  let _ = Sys.command (Printf.sprintf "rm -rf %s" root) in
+  let _ = Sys.command (Printf.sprintf "mkdir -p %s" root) in
+  ()
+
+(* Install the FS listener. *)
+let () = Irmin_unix.set_listen_dir_hook ()
+end
+
+let info = Irmin_unix.info
+
+module Store = Irmin_unix.Git.FS.KV (Irmin.Contents.String)
+
+let update t k v =
+  let msg = sprintf "Updating /%s" (String.concat "/" k) in
+  print_endline msg;
+  Store.set_exn t ~info:(info "%s" msg) k v
+
+let read_exn t k =
+  let msg = sprintf "Reading /%s" (String.concat "/" k) in
+  print_endline msg;
+  Store.get t k
+
+let main () =
+  Config.init ();
+  let config = Irmin_git.config ~bare:true Config.root in
+  Store.Repo.v config >>= fun repo ->
+  Store.master repo >>= fun t ->
+  update t [ "root"; "misc"; "1.txt" ] "Hello world!" >>= fun () ->
+  update t [ "root"; "misc"; "2.txt" ] "Hi!" >>= fun () ->
+  update t [ "root"; "misc"; "3.txt" ] "How are you ?" >>= fun () ->
+  read_exn t [ "root"; "misc"; "2.txt" ] >>= fun _ ->
+  Store.clone ~src:t ~dst:"test" >>= fun x ->
+  print_endline "cloning ...";
+  update t [ "root"; "misc"; "3.txt" ] "Hohoho" >>= fun () ->
+  update x [ "root"; "misc"; "2.txt" ] "Cool!" >>= fun () ->
+  Store.merge_into ~info:(info "t: Merge with 'x'") x ~into:t >>= function
+  | Error _ -> failwith "conflict!"
+  | Ok () ->
+      print_endline "merging ...";
+      read_exn t [ "root"; "misc"; "2.txt" ] >>= fun _ ->
+      read_exn t [ "root"; "misc"; "3.txt" ] >>= fun _ -> Lwt.return_unit
+
+let () =
+  Printf.printf
+    "This example creates a Git repository in %s and use it to read \n\
+     and write data:\n"
+    Config.root;
+  let _ = Sys.command (Printf.sprintf "rm -rf %s" Config.root) in
+  Lwt_main.run (main ());
+  Printf.printf "You can now run `cd %s && tig` to inspect the store.\n"
+    Config.root
