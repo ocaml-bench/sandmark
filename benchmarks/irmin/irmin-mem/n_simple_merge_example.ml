44258459c7107ae146b6d927740ba68e66235284
shubhamkumar13 <kumar.shubham1312@gmail.com>
2020-01-28 15:33:47 +0530

    modifications to n_simple_merge_example.ml file

--- benchmarks/irmin/irmin-mem/n_simple_merge_example.ml
+++ benchmarks/irmin/irmin-mem/n_simple_merge_example.ml
@@ -1,3 +1,4 @@
+
 let log_one =
    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id lacus at quam rutrum gravida. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec nisi ipsum, ultricies non enim id, facilisis iaculis magna. In et nisi volutpat, ultricies justo a, consectetur lorem. Integer odio lacus, suscipit ut nisi eu, vehicula commodo massa. Praesent rutrum dignissim est, nec sodales tellus elementum et. Etiam id nulla et lectus blandit dapibus. Cras sed mollis quam. Pellentesque gravida varius dolor, vulputate commodo metus condimentum id. Suspendisse placerat turpis eu pretium blandit. Aliquam eu purus sit amet mauris convallis pulvinar. Aliquam egestas augue ac lorem facilisis, consequat sodales erat pulvinar. Quisque sollicitudin placerat massa, ut aliquam eros porttitor quis.Nam posuere, risus molestie volutpat lacinia, libero nibh fringilla sem, sit amet suscipit ligula erat in nisl. Aenean pharetra libero et ex consectetur varius. Cras vulputate accumsan neque non dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas imperdiet, ex vel vehicula elementum, enim urna semper augue, quis auctor turpis magna id nunc. Nulla auctor tellus nec viverra luctus. Phasellus ut magna sed justo fermentum cursus. Sed bibendum erat nisi, vitae ornare justo gravida eu. Nulla facilisi. Quisque ac dapibus lorem, eget volutpat dui. Phasellus laoreet non lectus eu aliquet. Integer elementum, tellus in tincidunt placerat, justo mi convallis augue, at porttitor nunc tortor eu enim. Maecenas laoreet tortor at lacus feugiat, a ullamcorper ipsum varius. Etiam non diam ante.Duis purus enim, viverra a eleifend id, efficitur eget nunc. Suspendisse sed nibh tincidunt, elementum tortor a, consequat urna. Curabitur lacinia porta nulla, quis porta nunc vehicula in. Aliquam nec risus nisl. Nunc ullamcorper semper dignissim. Aliquam suscipit sapien et metus bibendum imperdiet. Mauris id metus libero. Nullam neque ipsum, tempus vitae mauris id, sollicitudin venenatis tortor. Vivamus at porttitor quam. Nulla ac ultricies metus. Integer in urna enim. Ut imperdiet odio sagittis sapien facilisis dictum. Nullam ultricies neque metus, vitae porttitor neque lobortis ac. Mauris quam magna, gravida a faucibus posuere, pellentesque sed eros. Quisque pharetra orci ac auctor euismod. Proin facilisis quam eu ipsum suscipit, ac dictum sem fermentum.Ut augue arcu, blandit nec nunc at, dictum porttitor massa. Pellentesque nec lorem massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque eget nunc rhoncus mi dignissim faucibus. Curabitur sit amet mi a leo cursus commodo. Donec sed accumsan sapien, eget ullamcorper dolor. Duis ut ultrices magna, eu finibus metus. Vivamus lobortis, augue sed luctus fringilla, tortor orci elementum orci, et viverra nunc tortor eget lectus. Nunc sagittis fermentum rhoncus. Proin placerat, eros sed placerat consequat, enim quam cursus urna, sit amet volutpat lorem velit eu sapien. Praesent et nulla a ligula sollicitudin convallis.Sed vulputate, lorem vitae sodales varius, metus justo gravida ligula, eu semper felis urna ac ante. Pellentesque nec bibendum enim, sit amet pretium sem. Curabitur vel ullamcorper diam. Etiam a nibh in sem tincidunt vestibulum in consectetur tellus. Morbi id augue id sapien pretium aliquam. Proin vel porttitor sem. Phasellus porta turpis id risus posuere, et dignissim augue ultrices. Vestibulum fringilla purus quis turpis elementum, ac malesuada diam gravida."
 
@@ -13,7 +14,8 @@ let log_two =
     Donec vehicula nunc diam, quis convallis arcu ultrices a. Nam fringilla, magna eu ornare rutrum, tellus ligula hendrerit nisi, vitae dictum eros leo eget lacus. Donec et varius mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras consectetur mollis metus vitae scelerisque. Morbi eget porta neque. Sed in odio malesuada, posuere turpis quis, vehicula urna. Ut tincidunt nunc accumsan posuere gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce semper, quam in vehicula vehicula, lectus ante condimentum libero, at blandit lorem odio sed lectus. Aenean at ante enim. Donec posuere, risus sed luctus vehicula, diam libero sodales magna, nec tristique justo elit tincidunt erat. Vivamus lacinia tellus eleifend augue consectetur tempor. Aliquam erat volutpat. Maecenas id tincidunt sem. Cras quis ex egestas augue fermentum fringilla. 
     Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque nec risus elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum sapien quis eleifend pharetra. Suspendisse iaculis eu sapien id tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus semper ligula a sollicitudin luctus."
 
-(* Simple example of reading and writing in a Git repository *)
+(* Simple example of reading and writing in a Git repository 
+   Although this only contains writing(100%) no reading *)
 open Lwt.Infix
 open Printf
 
@@ -58,14 +60,20 @@ let format (id : int) : string = sprintf "%s.txt" (string_of_int id)
       - merge into the master branch *)
 
 let custom_clone src dst content key_list =
-  let counter = ref 0 in  
-  let get_name x = counter := !counter + 1; (string_of_int counter.contents) ^ x in
+  (* counter to help us name branches and log files *)
+  let counter = ref 0 in
+
+  (* get_name gets us the name of the branch *)
+  let get_name () = counter := !counter + 1; string_of_int counter.contents ^ "_" ^ dst in
+
+  (* change_last gets us the name of the *)
   let change_last (x : string list) =
     let rec init x = match x with [] -> [] | _ :: [] -> [] | hd :: tl -> hd :: (init tl) in
     let last = List.rev x |> List.hd |> (^) (string_of_int !counter ^ "_") in
     let init_list = init x in
     List.append init_list [last]
   in
+
   let rec custom_clone' src (dst : string) (content : string list) (key_list : string list) =
     match content with
     | []       -> Lwt.return_unit
@@ -74,11 +82,12 @@ let custom_clone src dst content key_list =
                   Store.merge_into ~info:(info "t: Merge with 'x'") x ~into:src >>= function
                   | Error _ -> failwith "conflict"
                   | Ok ()   -> print_endline "merging ...";
-                  custom_clone' src (get_name dst) tl key_list
+                  custom_clone' src (get_name ()) tl key_list
     (* we generate our own dst and key_list 
       get_name produces the dst branch
       change_last genrates the new text file name *)
   in
+
   custom_clone' src dst (para_to_list content) key_list >>= fun () -> Lwt.return_unit
 
 let main () =
@@ -86,9 +95,9 @@ let main () =
   let config = Irmin_git.config ~bare:true Config.root in
   Store.Repo.v config >>= fun repo ->
   Store.master repo >>= fun t -> 
-  update t ["root"; "misc"; "1.txt"] log_one >>= fun () ->
-  custom_clone t "test" log_two ["root"; "misc" ; "sample.txt"] >>= fun () ->
-  read_exn t ["root"; "misc"; "72_sample.txt"] >>= fun _ -> Lwt.return_unit
+  update t ["root"; "misc"; "base.txt"] log_one >>= fun () ->
+  custom_clone t "branch" log_two ["root"; "misc" ; "log.txt"] >>= fun () ->
+  Lwt.return_unit
   
 let () =
   Printf.printf
