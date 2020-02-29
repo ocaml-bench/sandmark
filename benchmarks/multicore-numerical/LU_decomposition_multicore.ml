8fa856990877b229a14ef99e85a968ed92e57eb2
KC Sivaramakrishnan <kc@kcsrk.info>
2020-03-01 03:31:58 +0530

    Update parameters for LU and FW

--- benchmarks/multicore-numerical/LU_decomposition_multicore.ml
+++ benchmarks/multicore-numerical/LU_decomposition_multicore.ml
@@ -2,6 +2,13 @@ module C = Domainslib.Chan
 
 open Format
 
+module Array = struct
+  include Array
+
+  let init_matrix m n f = init m (fun i -> init n (f i))
+end
+
+
 type message = Do of (unit -> unit) | Quit
 
 type chan = {req: message C.t; resp: unit C.t}
@@ -10,31 +17,19 @@ let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
 
 let mat_size = try int_of_string Sys.argv.(2) with _ -> 1200
 
-let channels = 
-Array.init (num_domains -1) (fun _ -> {req= C.make 1; resp= C.make 0})
+let channels =
+Array.init (num_domains -1) (fun _ -> {req= C.make 1; resp= C.make 1})
 
-module Array = struct
-  include Array
-
-  let init_matrix m n f = init m (fun i -> init n (f i))
-
-  (** [swap x i j] swaps [x.(i)] and [x.(j)]. *)
-  let swap x i j =
-    let tmp = x.(i) in
-    x.(i) <- x.(j);
-    x.(j) <- tmp
-end
-
-let distribution = 
-  let rec loop n d acc = 
+let distribution =
+  let rec loop n d acc =
     if d = 1 then n::acc
-    else 
+    else
       let w = n / d in
       loop (n - w) (d - 1) (w::acc)
   in
   Array.of_list (loop mat_size num_domains [])
 
-let run_iter job = 
+let run_iter job =
   let sum = ref 0 in
   Array.iteri (fun i c ->
     let begin_ = !sum in
@@ -48,25 +43,22 @@ let run_iter job =
 let aux a k size s e =
   for row = s to (pred e) do
       match row >= k + 1  with
-      | true -> 
+      | true ->
         let factor = (a.(row).(k)) /. (a.(k).(k)) in
         for col = k + 1 to size-1 do
             Domain.Sync.poll();
             a.(row).(col) <- a.(row).(col) -. factor *. a.(k).(col)
         done;
         a.(row).(k) <- factor
-      | false -> ()  
-  done 
+      | false -> ()
+  done
 
 let lup a0 =
 let a = Array.copy a0 in
   for k = 0 to (mat_size - 2) do
     run_iter (fun s e () -> aux a k mat_size s e)
   done ;
-  a 
-
-
-
+  a
 
 let print_mat label x =
   printf "%s =@\n" label;
@@ -80,29 +72,28 @@ let rec worker c () =
   | Do f ->
       f () ; C.send c.resp () ; worker c ()
   | Quit ->
-      () 
+      ()
 
 
 let () =
   let domains = Array.map (fun c -> Domain.spawn (worker c)) channels in
- (*  let a = 
+ (*  let a =
   [|
     [| 1.; -2.; -2.; -3.|];
     [|3.; -9.; 0.; -9.|];
     [| -1.; 2.;4.; 7.|];
     [| -3.; -6.; 26.; 2.|];
-    
+
   |]  in *)
-  let a = Array.init mat_size 
+  let a : float array array = Array.init mat_size
   (fun _ -> Array.init mat_size (fun _ -> (Random.float 100.0)+.1.0)) in
   (* print_mat "matrix A" a ; *)
-  let lu = lup a in (* in
-  let l = Array.init_matrix mat_size mat_size
+  let lu = lup a in
+  let _l = Array.init_matrix mat_size mat_size
       (fun i j -> if i > j then lu.(i).(j) else if i = j then 1.0 else 0.0) in
-  let u = Array.init_matrix mat_size mat_size
-      (fun i j -> if i <= j then lu.(i).(j) else 0.0) in *)
+  let _u = Array.init_matrix mat_size mat_size
+      (fun i j -> if i <= j then lu.(i).(j) else 0.0) in
   (* print_mat "matrix L" l;
   print_mat "matrix U" u; *)
-  ignore lu ;
   Array.iter (fun c -> C.send c.req Quit) channels ;
-  Array.iter Domain.join domains 
+  Array.iter Domain.join domains
--- benchmarks/multicore-numerical/floyd_warshall_multicore.ml
+++ benchmarks/multicore-numerical/floyd_warshall_multicore.ml
@@ -1,19 +1,20 @@
-(* module Graph = struct *)
-(* type edge = {sv: int ; ev: int; weight: int} *)
+let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
+let n = try int_of_string Sys.argv.(2) with _ -> 4
+(* n is size of matrix  *)
 
 let sum x y =
-  match x , y with 
+  match x , y with
   | Some x ,Some y -> Some (x+y)
   | _ , _-> None
 
-let print_inf x = 
+let print_inf x =
   match  x with
   | Some x -> print_int x
-  | None -> print_string "∞" 
+  | None -> print_string "∞"
 
-let print_mat adjacency = 
-print_endline " ";  
-let rows = Array.length adjacency in 
+let print_mat adjacency =
+print_endline " ";
+let rows = Array.length adjacency in
 let columns = Array.length adjacency.(0) in
    for i = 0 to (rows - 1) do
        for j = 0 to (columns - 1) do
@@ -22,53 +23,41 @@ let columns = Array.length adjacency.(0) in
        print_endline " "
    done
 
-
 module C = Domainslib.Chan
-
 type message = Do of (unit -> unit) | Quit
-
 type chan = {req: message C.t; resp: unit C.t}
-
-let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
-
-let n = try int_of_string Sys.argv.(2) with _ -> 4
-(* n is size of matrix  *)
-let channels = 
-Array.init (num_domains -1) (fun _ -> {req= C.make 1; resp= C.make 0})
+let channels =
+  Array.init (num_domains -1) (fun _ -> {req= C.make 1; resp= C.make 1})
 
 let my_formula () =
   let r = Random.int 100 in
-  let r1 = Random.int 2 in 
+  let r1 = Random.int 2 in
   match r1 with
   |0 ->None
   |_-> Some r
-  
 
-let adj = Array.init n (fun _ -> Array.init n (fun _ -> my_formula ())) 
+let adj = Array.init n (fun _ -> Array.init n (fun _ -> my_formula ()))
 
 let edit_diagonal mat =
   Array.iteri (fun i _ -> mat.(i).(i) <- Some 0) mat
 
-  
 (* let adj = [|
     [| Some 0; Some 8;None; Some 1 |];
     [| None; Some 0; Some 1; None|];
     [| Some 4;None;Some 0;None |];
     [| None; Some 2; Some 9;Some 0 |];
-  |] *)   
+  |] *)
 
-
-let distribution = 
-  let rec loop n d acc = 
+let distribution =
+  let rec loop n d acc =
     if d = 1 then n::acc
-    else 
+    else
       let w = n / d in
       loop (n - w) (d - 1) (w::acc)
   in
   Array.of_list (loop n num_domains [])
 
-
-let run_iter job = 
+let run_iter job =
   let sum = ref 0 in
   Array.iteri (fun i c ->
     let begin_ = !sum in
@@ -77,8 +66,6 @@ let run_iter job =
     C.send c.req (Do (job begin_ end_))) channels;
   job !sum (!sum + distribution.(num_domains - 1)) ();
   Array.iter (fun c -> C.recv c.resp) channels
-
-
 
 let f_w s e k =
     for i = s to (pred e) do
@@ -90,7 +77,6 @@ let f_w s e k =
         done
     done;
     ()
-  
 
 let aux () =
   for k = 0 to (pred n) do
@@ -104,7 +90,7 @@ let rec worker c () =
   | Quit ->
       ()
 
-let ()= 
+let ()=
   edit_diagonal adj;
   let domains = Array.map (fun c -> Domain.spawn (worker c)) channels in
   aux() ;
