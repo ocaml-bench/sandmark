216de86fca56d2d5d35124d2c4f4a9f376018fca
KC Sivaramakrishnan <kc@kcsrk.info>
2020-03-02 10:05:23 +0530

    Add the make run config json step to run_all scripts

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run `make run_config_macro.json` before running this script.
+make run_config_macro.json
 
 RUN_CONFIG_JSON=run_config_macro.json make ocaml-versions/4.06.1+multicore.bench
 RUN_BENCH_TARGET=run_pausetimes_multicore RUN_CONFIG_JSON=run_config_macro.json make ocaml-versions/4.06.1+multicore+pausetimes.bench
