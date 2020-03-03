5946ed0145251396782b940ea36bcf7c0f733fc8
KC Sivaramakrishnan <kc@kcsrk.info>
2020-03-04 02:43:40 +0530

    Add a missing poll for binary trees

--- benchmarks/multicore-numerical/binarytrees5_multicore.ml
+++ benchmarks/multicore-numerical/binarytrees5_multicore.ml
@@ -18,7 +18,11 @@ let rec make d =
   if d = 0 then Node(Empty, Empty)
   else let d = d - 1 in Node(make d, make d)
 
-let rec check = function Empty -> 0 | Node(l, r) -> 1 + check l + check r
+let rec check t =
+  Domain.Sync.poll ();
+  match t with
+  | Empty -> 0
+  | Node(l, r) -> 1 + check l + check r
 
 let min_depth = 4
 let max_depth = max (min_depth + 2) max_depth
