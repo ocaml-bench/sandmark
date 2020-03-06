facdfb98d632a932b3533d2d17cc6dd190daa2a9
Anmol Sahoo <anmol.sahoo25@gmail.com>
2020-03-06 05:49:29 +0530

    irmin benchmark updated and run added

--- benchmarks/irmin/basic_read_write.ml
+++ benchmarks/irmin/basic_read_write.ml
@@ -1,9 +1,10 @@
 module KV = Irmin_mem.KV (Irmin.Contents.String)
 
 let _ =
-  let _c = Irmin_mem.config () in
-  let _r = KV.Repo.v _c in
-  let _p1 = Lwt.bind _r (fun x -> KV.master x) in
-  let _p2 = Lwt.bind _p1 (fun x -> KV.set x ["test"] ~info:(Irmin.Info.none) "test1") in
-  let _p3 = Lwt.bind _p1 (fun x -> KV.get x ["test"]) in
-  print_endline (Lwt_main.run _p3)
+  let s = Sys.argv.(1) in
+  let c = Irmin_mem.config () in
+  let r = KV.Repo.v c in
+  let p1 = Lwt.bind r (fun x -> KV.master x) in
+  let _ = Lwt.bind p1 (fun x -> KV.set x ["test"] ~info:(Irmin.Info.none) s) in
+  let p3 = Lwt.bind p1 (fun x -> KV.get x ["test"]) in
+  assert (Lwt_main.run p3 = s)
