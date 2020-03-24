134cf6c2244e4529ade6d7680f081d017c14342e
KC Sivaramakrishnan <kc@kcsrk.info>
2020-03-24 13:53:24 +0530

    Change default chunksize to 32

--- benchmarks/multicore-grammatrix/grammatrix_multicore.ml
+++ benchmarks/multicore-grammatrix/grammatrix_multicore.ml
@@ -12,7 +12,7 @@ module C = Domainslib.Chan
 
 let num_domains = try int_of_string Sys.argv.(1) with _ -> 4
 let input_fn = try Sys.argv.(2) with _ ->  "data/tox21_nrar_ligands_std_rand_01.csv"
-let chunk_size = try int_of_string Sys.argv.(3) with _ -> 16
+let chunk_size = try int_of_string Sys.argv.(3) with _ -> 32
 
 let dot_product xs ys =
   let n = A.length xs in
