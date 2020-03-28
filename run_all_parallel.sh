4a2ff544b29ac14ba7a7237de8bdc35c5c84817d
KC Sivaramakrishnan <kc@kcsrk.info>
2020-03-28 15:12:54 +0530

    Update instructions for running parallel benchmarks

--- run_all_parallel.sh
+++ run_all_parallel.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Since the parallel benchmarks use `chrt -r 1` you will need to run this script
+# twice. First run without sudo which will build the compiler and the benchmarks,
+# and will fail running the benchmarks. Then run the script with sudo which will
+# run the benchmarks and produce the results.
+#
+# $ bash run_all_parallel.sh; sudo bash run_all_parallel.sh
+
 make multicore_parallel_run_config_macro.json
 
 RUN_BENCH_TARGET=run_pausetimes_multicore BUILD_BENCH_TARGET=multibench_parallel \
@@ -15,5 +22,3 @@ BUILD_BENCH_TARGET=multibench_parallel \
 BUILD_BENCH_TARGET=multibench_parallel \
 	RUN_CONFIG_JSON=multicore_parallel_run_config_macro.json \
 	make ocaml-versions/4.06.1+multicore+stw+parallel.bench
-
-
