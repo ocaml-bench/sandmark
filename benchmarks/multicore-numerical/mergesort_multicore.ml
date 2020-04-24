07119654b623bf7a3e26368cf9f21f38ee7d3820
shubhamkumar13 <kumar.shubham1312@gmail.com>
2020-04-24 19:10:39 +0530

    wip-double-buffer-mergesort-parallel

--- benchmarks/multicore-numerical/mergesort_multicore.ml
+++ benchmarks/multicore-numerical/mergesort_multicore.ml
@@ -1,66 +1,86 @@
 module T = Domainslib.Task
 
-let num_domains = try int_of_string @@ Sys.argv.(1) with _ -> 4
+let num_domains = try int_of_string @@ Sys.argv.(1) with _ -> 1
+let n = try int_of_string @@ Sys.argv.(2) with _ -> 1024
+let min = 128
+let pool = T.setup_pool ~num_domains:(num_domains - 1)
 
-let n = try int_of_string @@ Sys.argv.(2) with _ ->  10000
-
-let domain_pool = T.setup_pool ~num_domains:(num_domains - 1)
-
-let rec take n = function
-  | [] -> []
-  | x::xs -> if (n > 0) then x :: (take (n-1) xs) else []
-
-let rec drop n = function
-  | [] -> []
-  | x::xs -> if (n = 0) then x::xs
-    else if (n > 0) then (drop (n-1) xs)
-    else []
-
-let slice lst start range = take range (drop start lst)
-
-let rec merge x y =
-  match x, y with
-    | [], l -> l
-    | l, [] -> l
-    | x :: xs , y :: ys ->
-      if x < y
-          then x :: merge xs (y :: ys)
-          else y :: merge (x::xs) ys
+let _ = Random.init 42
+let a = Array.init n (fun _ -> Random.int n)
+let b = Array.make n 0
 
 
-let rec halving l =
-  match l with
-  | [] -> []
-  | [x] -> [x]
-  | [x;y] -> if (x < y) then [x;y] else [y;x]
-  | _ ->
-    let left = take (List.length l/2) l in
-    let right = drop (List.length l/2) l in
-    merge (halving left) (halving right)
+type array_slice = {arr: int array; index: int; length: int}
 
-let rec msort l =
-  match l with
-  | [] -> []
-  | [x] -> [x]
-  | _ ->
-    let left = take (List.length l/2) l in
-    let right = drop (List.length l/2) l in
-    (* Printf.printf "I'm merging\n"; *)
-    merge (msort left) (msort right)
+let print_array_slice s a =
+  print_string (s^"=");
+  for i = a.index to a.index + a.length - 1 do
+    Printf.printf "%d " a.arr.(i)
+  done;
+  print_endline ""
 
+let sort a =
+  for i = a.index to a.index + a.length - 2 do
+    for j = i + 1 to a.index + a.length - 1 do
+      if a.arr.(j) < a.arr.(i) then
+        let t = a.arr.(i) in
+        a.arr.(i) <- a.arr.(j);
+        a.arr.(j) <- t
+    done
+  done
 
-let main () =
-    let arr = Array.init n (fun _ -> Random.int 100) in 
-    (* List.iter (fun el ->
-        List.iter (fun x -> Printf.printf " %d " x) el;
-        print_endline ""; ) new_lst *)
-    let _lst =
-        List.map (fun l -> T.async domain_pool (fun _ -> msort l)) lst |>
-        List.map (fun l -> T.await domain_pool l) |>
-        msort |>
-        List.fold_left (fun acc x -> acc @ x) [] in
-    (* List.iter (fun e -> Printf.printf " %d " e) lst;
-    print_endline "" *)
-        ()
+let merge a b res =
+  let rec loop ai bi ri =
+    match a.index + a.length - ai, b.index + b.length - bi with
+    | n, 0 -> Array.blit a.arr ai res.arr ri n
+    | 0, n -> Array.blit b.arr bi res.arr ri n
+    | _, _ ->
+        if a.arr.(ai) < b.arr.(bi) then begin
+          res.arr.(ri) <- a.arr.(ai);
+          loop (ai+1) bi (ri+1)
+        end else begin
+          res.arr.(ri) <- b.arr.(bi);
+          loop ai (bi+1) (ri+1)
+        end
+  in
+  loop a.index b.index res.index
 
-let _ = main ()
+let rec merge_sort a b l =
+  if a.length <= min then begin
+    sort a;
+    a
+  end else
+    let a1= {a with index = a.index; length = a.length / 2} in
+    let b1 = {b with index = b.index; length = b.length / 2} in
+    let r1 = T.async pool (fun _ -> merge_sort a1 b1 (2*l+1)) in
+
+    let a2 = {a with index = a.index + a.length / 2;
+                length = a.length - a.length / 2} in
+    let b2 = {b with index = b.index + b.length / 2;
+                length = b.length - b.length / 2} in
+    let r2 = T.async pool (fun _ -> merge_sort a2 b2 (2*l+2)) in
+
+    let (r1, r2) = (T.await pool r1, T.await pool r2) in
+
+    if r1.arr != r2.arr then begin
+      if r2.arr == a.arr then begin
+        merge r1 r2 a;
+        a
+      end else begin
+        merge r1 r2 b;
+        b
+      end
+    end else if r1.arr == a.arr then begin
+      merge r1 r2 b;
+      b
+    end else begin
+      merge r1 r2 a;
+      a
+    end
+
+let _ =
+  let aslice = {arr = a; index = 0; length = n}in
+  let bslice = {arr = b; index = 0; length = n} in
+
+  let _r = merge_sort aslice bslice 0 in
+  T.teardown_pool pool
