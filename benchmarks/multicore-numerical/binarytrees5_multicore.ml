f383596bef47075c687f375a38d3a4361e19f30e
shubhamkumar13 <kumar.shubham1312@gmail.com>
2020-05-04 11:44:45 +0530

    Task-api port of Game of Life, Spectralnorm, Binarytree5
    minor edits

--- benchmarks/multicore-numerical/binarytrees5_multicore.ml
+++ benchmarks/multicore-numerical/binarytrees5_multicore.ml
@@ -1,15 +1,8 @@
-module C = Domainslib.Chan
-
-type message = Do of (unit -> unit) | Quit
-
-type chan = {req: message C.t; resp: unit C.t}
+module T = Domainslib.Task
 
 let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
 let max_depth = try int_of_string Sys.argv.(2) with _ -> 10
-
-let channels =
-  Array.init (num_domains - 1) (fun _ -> {req= C.make_bounded 1; resp= C.make_bounded 1})
-
+let pool = T.setup_pool ~num_domains:(num_domains - 1)
 
 type 'a tree = Empty | Node of 'a tree * 'a tree
 
@@ -30,12 +23,14 @@ let stretch_depth = max_depth + 1
 
 let () =
   (* Gc.set { (Gc.get()) with Gc.minor_heap_size = 1024 * 1024; max_overhead = -1; }; *)
-  let c = check (make stretch_depth) in
-  Printf.printf "stretch tree of depth %i\t check: %i\n" stretch_depth c
+  let _ = check (make stretch_depth) in
+  ()
+  (* Printf.printf "stretch tree of depth %i\t check: %i\n" stretch_depth c *)
 
 let long_lived_tree = make max_depth
 
 let values = Array.make num_domains 0
+let promise_list = ref []
 
 let calculate d st en ind =
   (* Printf.printf "st = %d en = %d\n" st en; *)
@@ -43,36 +38,28 @@ let calculate d st en ind =
   for _ = st to en do
   c := !c + check (make d)
   done;
+  (* Printf.printf "ind = %d\n" ind; *)
   values.(ind) <- !c
-
-let rec worker c () =
-  match C.recv c.req with
-  | Do f ->
-      f () ; C.send c.resp () ; worker c ()
-  | Quit ->
-      ()
 
 let loop_depths d =
   for i = 0 to  ((max_depth - d) / 2 + 1) - 1 do
     let d = d + i * 2 in
     let niter = 1 lsl (max_depth - d + min_depth) in
-    let job i () = calculate d (i * niter / num_domains) (((i + 1) * niter / num_domains) - 1) i in
-    Array.iteri (fun i c -> C.send c.req (Do (job i))) channels ;
-    job (num_domains - 1) ();
-    Array.iter (fun c -> C.recv c.resp) channels;
-    let sum = Array.fold_left (+) 0 values in
+    for i = 0 to pred num_domains do
+      promise_list := (T.async pool (fun _ -> calculate d (i * niter / num_domains) (((i + 1) * niter / num_domains) - 1) i )) :: !promise_list
+    done;
 
-    (* let domains = worker d (num_domains) 1 (niter/num_domains) in
-    let sum = List.fold_left (+) 0 (List.map Domain.join domains) in *)
-    (* let sum = calculate d 1 niter in *)
-      Printf.printf "%i\t trees of depth %i\t check: %i\n" niter d sum ;
+    List.iter (fun pr -> T.await pool pr) (List.rev !promise_list);
+    
+    (* T.parallel_for pool ~chunk_size:(num_domains) ~start:0 ~finish:(num_domains - 1) ~body:(fun index -> calculate d ((index * niter) / num_domains) ((((index + 1) * niter) / num_domains) - 1) index); *)
+
+    let _sum = Array.fold_left (+) 0 values in
+    ()
+    (* Printf.printf "%i\t trees of depth %i\t check: %i\n" niter d sum *)
   done
 
 let () =
-  let domains = Array.map (fun c -> Domain.spawn (worker c)) channels in
-  flush stdout;
   loop_depths min_depth;
-  Printf.printf "long lived tree of depth %i\t check: %i\n"
-    max_depth (check long_lived_tree);
-    Array.iter (fun c -> C.send c.req Quit) channels ;
-    Array.iter Domain.join domains
+  let _ = max_depth in
+  let _ = (check long_lived_tree) in
+  T.teardown_pool pool
--- benchmarks/multicore-numerical/spectralnorm2_multicore.ml
+++ benchmarks/multicore-numerical/spectralnorm2_multicore.ml
@@ -6,77 +6,43 @@
  * Modified by Mauricio Fernandez
  *)
 
-module C = Domainslib.Chan
-
 let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
-let n = try int_of_string Sys.argv.(2) with _ -> 2000
+let n = try int_of_string Sys.argv.(2) with _ ->  2000
 
-type message = Do of (unit -> unit) | Quit
-type chan = {req: message C.t; resp: unit C.t}
+module T = Domainslib.Task
 
-let channels =
-  Array.init (num_domains - 1) (fun _ -> {req= C.make_bounded 1; resp= C.make_bounded 1})
+let eval_A i j = 1. /. float((i+j)*(i+j+1)/2+i+1)
 
-let eval_A i j = 1. /. float (((i + j) * (i + j + 1) / 2) + i + 1)
-
-let eval_A_times_u u v s e =
+let eval_A_times_u pool u v =
   let n = Array.length v - 1 in
-  for i = s to e do
+  T.parallel_for pool ~start:0 ~finish:n ~chunk_size:(n/num_domains)
+    ~body:(fun i ->
+      let vi = ref 0. in
+      for j = 0 to n do vi := !vi +. eval_A i j *. u.(j) done;
+      v.(i) <- !vi)
+
+let eval_At_times_u pool u v =
+  let n = Array.length v -1 in
+  T.parallel_for pool ~start:0 ~finish:n ~chunk_size:(n/num_domains)
+    ~body:(fun i ->
     let vi = ref 0. in
-    for j = 0 to n do
-      vi := !vi +. (eval_A i j *. u.(j))
-    done ;
-    v.(i) <- !vi
-  done
+    for j = 0 to n do vi := !vi +. eval_A j i *. u.(j) done;
+    v.(i) <- !vi)
 
-let divvy_up1 u v =
-  let job i () =
-    eval_A_times_u u v (i * n / num_domains) (((i + 1) * n / num_domains) - 1)
-  in
-  Array.iteri (fun i c -> C.send c.req (Do (job i))) channels ;
-  job (num_domains - 1) ();
-  Array.iter (fun c -> C.recv c.resp) channels
-
-let eval_At_times_u u v s e =
-  let n = Array.length v - 1 in
-  for i = s to e do
-    let vi = ref 0. in
-    for j = 0 to n do
-      vi := !vi +. (eval_A j i *. u.(j))
-    done ;
-    v.(i) <- !vi
-  done
-
-let divvy_up2 u v =
-  let job i () =
-    eval_At_times_u u v (i * n / num_domains) (((i + 1) * n / num_domains) - 1)
-  in
-  Array.iteri (fun i c -> C.send c.req (Do (job i))) channels ;
-  job (num_domains - 1) ();
-  Array.iter (fun c -> C.recv c.resp) channels
-
-let eval_AtA_times_u u v =
+let eval_AtA_times_u pool u v =
   let w = Array.make (Array.length u) 0.0 in
-  divvy_up1 u w ; divvy_up2 w v
-
-let rec worker c () =
-  match C.recv c.req with
-  | Do f ->
-      f () ; C.send c.resp () ; worker c ()
-  | Quit ->
-      ()
+  eval_A_times_u pool u w; eval_At_times_u pool w v
 
 let () =
-  let domains = Array.map (fun c -> Domain.spawn (worker c)) channels in
-  let u = Array.make n 1.0 and v = Array.make n 0.0 in
+  let pool = T.setup_pool ~num_domains:(num_domains - 1) in
+  let u = Array.make n 1.0  and  v = Array.make n 0.0 in
   for _i = 0 to 9 do
-    eval_AtA_times_u u v ; eval_AtA_times_u v u
-  done ;
-  let vv = ref 0.0 and vBv = ref 0.0 in
-  for i = 0 to n - 1 do
-    vv := !vv +. (v.(i) *. v.(i)) ;
-    vBv := !vBv +. (u.(i) *. v.(i))
-  done ;
-  Array.iter (fun c -> C.send c.req Quit) channels ;
-  Array.iter Domain.join domains ;
-  Printf.printf "%0.9f\n" (sqrt (!vBv /. !vv))
+    eval_AtA_times_u pool u v; eval_AtA_times_u pool v u
+  done;
+  T.teardown_pool pool;
+
+  let vv = ref 0.0  and  vBv = ref 0.0 in
+  for i=0 to n-1 do
+    vv := !vv +. v.(i) *. v.(i);
+    vBv := !vBv +. u.(i) *. v.(i)
+  done
