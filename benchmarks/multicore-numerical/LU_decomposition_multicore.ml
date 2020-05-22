515a537f89c52f4e98bb5a71ebb7f079c1ec252e
Sudha Parimala <sudharg247@gmail.com>
2020-05-22 16:24:43 +0530

    rename functions

--- benchmarks/multicore-numerical/LU_decomposition_multicore.ml
+++ benchmarks/multicore-numerical/LU_decomposition_multicore.ml
@@ -5,13 +5,13 @@ let chunk_size = try int_of_string Sys.argv.(3) with _ -> 16
 
 module SquareMatrix = struct
 
-  let create_seq f : float array =
+  let create f : float array =
     let fa = Array.create_float (mat_size * mat_size) in
     for i = 0 to mat_size * mat_size - 1 do
       fa.(i) <- f (i / mat_size) (i mod mat_size)
     done;
     fa
-  let create pool f : float array =
+  let parallel_create pool f : float array =
     let fa = Array.create_float (mat_size * mat_size) in
     T.parallel_for pool ~chunk_size:(mat_size * mat_size / num_domains) ~start:0
     ~finish:( mat_size * mat_size - 1) ~body:(fun i ->
@@ -20,9 +20,9 @@ module SquareMatrix = struct
 
   let get (m : float array) r c = m.(r * mat_size + c)
   let set (m : float array) r c v = m.(r * mat_size + c) <- v
-  let par_copy pool a = 
+  let parallel_copy pool a = 
     let n = Array.length a in
-    let part a b i = 
+    let copy_part a b i = 
       let s = (i * n / num_domains) in
       let e = (i+1) * n / num_domains - 1 in
       Array.blit a s b s (e - s + 1) in
@@ -31,17 +31,17 @@ module SquareMatrix = struct
       if (i = num_domains) then
         (List.iter (fun e -> T.await pool e) acc)
       else begin
-        aux ((T.async pool (fun _ -> part a b i))::acc) num_domains (i+1)
+        aux ((T.async pool (fun _ -> copy_part a b i))::acc) num_domains (i+1)
       end
-      in
-      aux [] num_domains 0;
-      b
+    in
+    aux [] num_domains 0;
+    b
 end
 
 open SquareMatrix
 
 let lup pool (a0 : float array) =
-  let a = par_copy pool a0 in
+  let a = parallel_copy pool a0 in
   for k = 0 to (mat_size - 2) do
   T.parallel_for pool ~chunk_size:chunk_size ~start:(k + 1) ~finish:(mat_size  -1)
   ~body:(fun row ->
@@ -55,8 +55,8 @@ let lup pool (a0 : float array) =
 
 let () =
   let pool = T.setup_pool ~num_domains:(num_domains - 1) in
-  let a = create_seq (fun _ _ -> (Random.float 100.0) +. 1.0 ) in (* (Random.float 100.0)+.1.0*)
+  let a = create (fun _ _ -> (Random.float 100.0) +. 1.0 ) in 
   let lu = lup pool a in
-  let _l = create pool (fun i j -> if i > j then get lu i j else if i = j then 1.0 else 0.0) in
-  let _u = create pool (fun i j -> if i <= j then get lu i j else 0.0) in
+  let _l = parallel_create pool (fun i j -> if i > j then get lu i j else if i = j then 1.0 else 0.0) in
+  let _u = parallel_create pool (fun i j -> if i <= j then get lu i j else 0.0) in
   T.teardown_pool pool
