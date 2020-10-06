34a776af501138647feeba557549ca5cbd91d61b
Anmol Sahoo <anmol.sahoo25@gmail.com>
2020-10-06 23:01:49 +0530

    added DLS

--- benchmarks/multicore-lockfree/test_ctrie.ml
+++ benchmarks/multicore-lockfree/test_ctrie.ml
@@ -2,16 +2,18 @@ let threads = int_of_string Sys.argv.(1)
 let insert_percent = int_of_string Sys.argv.(2)
 let num_opers = int_of_string Sys.argv.(3) / threads
 
-let () = Random.init 42
+let state_key = Domain.DLS.new_key Random.State.make_self_init
 
 let tree = Ctrie.empty ()
 
 let work () =
+  let state = Domain.DLS.get state_key in
+  let rand_int n = Random.State.int state n in
   for _ = 1 to num_opers do
-    if Random.int 100 > insert_percent then
-      ignore (Ctrie.mem (Random.int 1000000) tree)
+    if rand_int 100 > insert_percent then
+      ignore (Ctrie.mem (rand_int 1000000) tree)
     else
-      Ctrie.insert (Random.int 1000000) 0 tree 
+      Ctrie.insert (rand_int 1000000) 0 tree 
   done
 
 let () =
