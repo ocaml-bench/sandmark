7cd6a0655ab7bbb33b1ef555f4d3f7d4836b40e0
Sadiq <sadiq@sherwood.ocl.cl.cam.ac.uk>
2021-03-08 10:18:54 +0000

    don't need the compiler change now

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -2,9 +2,9 @@
 
 TAG='"macro_bench"' make run_config_filtered.json
 
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+multicore.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+multicore+mimalloc.bench
+RUN_BENCH_TARGET=run_orun_bf80 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock.bench
+RUN_BENCH_TARGET=run_orun_bf100 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock.bench
+RUN_BENCH_TARGET=run_orun_bf120 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock.bench
 
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock+bestfit80.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock+bestfit100.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock+bestfit120.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+multicore.bench
+RUN_BENCH_TARGET=run_orun_mimalloc RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+multicore.bench
