4e73b19c9c8c9303855ec85ab3c38c2f95828d4c
KC Sivaramakrishnan <kc@kcsrk.info>
2021-04-30 12:27:17 +0530

    Update run_all_parallel.sh

--- run_all_parallel.sh
+++ run_all_parallel.sh
@@ -8,22 +8,15 @@
 
 TAG='"macro_bench"' make multicore_parallel_run_config_filtered.json
 
-RUN_BENCH_TARGET=run_pausetimes_multicore BUILD_BENCH_TARGET=multibench_parallel \
-	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
-	make ocaml-versions/4.06.1+multicore+pausetimes+parallel.bench
-RUN_BENCH_TARGET=run_pausetimes_multicore BUILD_BENCH_TARGET=multibench_parallel \
-	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
-	make ocaml-versions/4.06.1+multicore+stw+pausetimes+parallel.bench
-RUN_BENCH_TARGET=run_pausetimes_multicore BUILD_BENCH_TARGET=multibench_parallel \
-	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
-	make ocaml-versions/4.10.0+multicore+pausetimes+parallel.bench
-
-BUILD_BENCH_TARGET=multibench_parallel \
+RUN_BENCH_TARGET=run_orunchrt \
+	BUILD_BENCH_TARGET=multibench_parallel \
 	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
 	make ocaml-versions/4.06.1+multicore+parallel.bench
-BUILD_BENCH_TARGET=multibench_parallel \
+RUN_BENCH_TARGET=run_orunchrt \
+	BUILD_BENCH_TARGET=multibench_parallel \
 	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
 	make ocaml-versions/4.06.1+multicore+stw+parallel.bench
-BUILD_BENCH_TARGET=multibench_parallel \
+RUN_BENCH_TARGET=run_orunchrt \
+	BUILD_BENCH_TARGET=multibench_parallel \
 	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
 	make ocaml-versions/4.10.0+multicore+parallel.bench
