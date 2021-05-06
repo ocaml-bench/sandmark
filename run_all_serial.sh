b178d24bfaa0b31cb4734eac8d05cce724801b88
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-05-06 14:11:43 +0530

    update automation script

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -2,8 +2,6 @@
 
 TAG='"macro_bench"' make run_config_filtered.json
 
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.06.1+multicore.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.06.1+multicore+stw.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.06.1+stock.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+multicore.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.10.0+stock.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+stock.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains+effects.bench
