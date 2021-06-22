f30011e4e665dcef0e396b764cfc682913ada892
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-06-22 18:00:39 +0530

    revert the modification to multicore_parallel_run_config.json

--- run_all_parallel.sh
+++ run_all_parallel.sh
@@ -6,13 +6,13 @@
 #   sudo setcap cap_sys_nice=ep /usr/bin/chrt
 #
 
-TAG='"macro_bench"' make multicore_parallel_iitm_run_config_filtered.json
+TAG='"macro_bench"' make multicore_parallel_run_config_filtered.json
 
 RUN_BENCH_TARGET=run_orunchrt \
 	BUILD_BENCH_TARGET=multibench_parallel \
-	RUN_CONFIG_JSON=multicore_parallel_iitm_run_config_filtered.json \
+	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
 	make ocaml-versions/4.12.0+domains.bench
 RUN_BENCH_TARGET=run_orunchrt \
 	BUILD_BENCH_TARGET=multibench_parallel \
-	RUN_CONFIG_JSON=multicore_parallel_iitm_run_config_filtered.json \
+	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
 	make ocaml-versions/4.12.0+domains+effects.bench
