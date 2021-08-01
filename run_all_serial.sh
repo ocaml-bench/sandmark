458b7079ee0d3643a3ec4cf1bedfd5576dcbbb32
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-08-01 22:48:38 +0530

    comment out alias to avoid building benchmarks instead of removing
    remove `run_in_ci` tag and `macro_bench` tag from failing benchmarks

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TAG='"macro_bench"' make run_config_filtered.json
+TAG='"run_in_ci"' make run_config_filtered.json
 
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+stock.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains+effects.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.14.0+trunk.bench
