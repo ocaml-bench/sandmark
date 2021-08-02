701350a6678330368085d659044deedd22bb3bf7
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-08-02 17:28:04 +0530

    revert ;; and remove the changes from run_all_serial.sh

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-TAG='"run_in_ci"' make run_config_filtered.json
+TAG='"macro_bench"' make run_config_filtered.json
 
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.14.0+trunk.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+stock.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains.bench
+RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains+effects.bench
