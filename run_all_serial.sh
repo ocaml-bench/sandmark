9a908f05b589fe171fa0be7978221df04d570057
Shakthi Kannan <author@shakthimaan.com>
2022-02-03 22:18:34 +0530

    Updated run_all scripts and 5.00.0+stable to February 3, 2022 commit

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -2,5 +2,4 @@
 
 TAG='"macro_bench"' make run_config_filtered.json
 
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/5.00.0+trunk.bench
-RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.14.0+domains.bench
+USE_SYS_DUNE_HACK=1 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/5.00.0+stable.bench
