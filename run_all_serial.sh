58b238c0bade165948842da7ac597d20054613f2
Moazzam Moriani <mozmoriani@gmail.com>
2022-04-26 16:13:50 +0500

    Pin base to patched version

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -2,4 +2,4 @@
 
 TAG='"macro_bench"' make run_config_filtered.json
 
-OPT_WAIT=0 USE_SYS_DUNE_HACK=1 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/5.0.0+stable.bench
+USE_SYS_DUNE_HACK=1 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/5.0.0+stable.bench
