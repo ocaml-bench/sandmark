cb9f350b468d6fbee631a41e30a519ed4e1b47be
KC Sivaramakrishnan <kc@kcsrk.info>
2023-02-28 10:23:43 +0530

    Update run_all_serial.sh
    Remove dev branch

--- run_all_serial.sh
+++ run_all_serial.sh
@@ -6,7 +6,3 @@ OPT_WAIT=0 USE_SYS_DUNE_HACK=1 \
 	SANDMARK_CUSTOM_NAME=5.1.0+trunk \
 	RUN_CONFIG_JSON=run_config_filtered.json \
 	make ocaml-versions/5.1.0+trunk.bench
-OPT_WAIT=0 USE_SYS_DUNE_HACK=1 \
-	SANDMARK_CUSTOM_NAME=5.1.0+trunk+decouple_gc \
-	RUN_CONFIG_JSON=run_config_filtered.json \
-	make ocaml-versions/5.1.0+trunk+decouple_gc.bench
