9dfda5f03c634067ae09bec08a97d68e9fafacc7
Ambre Austen Suhamy <ambre@tarides.com>
2023-05-26 15:57:01 +0200

    Linting toplevel shell scripts

--- run_all_custom.sh
+++ run_all_custom.sh
@@ -8,12 +8,12 @@ TMP_CUSTOM_FILE=$(mktemp --tmpdir custom.XXXXXXXX.json)
 TMP_DIR="/tmp"
 
 # Host
-HOSTNAME=`hostname`
+HOSTNAME=$(hostname)
 
 # Functions
 check_not_expired () {
-    EXPIRY_DATE=$(date -d $1 +%s)
-    TODAY=`date +%Y-%m-%d`
+    EXPIRY_DATE=$(date -d "$1" +%s)
+    TODAY=$(date +%Y-%m-%d)
     CURRENT_DATE=$(date -d "${TODAY}" +%s)
     if [[ "${CURRENT_DATE}" -le "${EXPIRY_DATE}" ]]; then
         return 0
@@ -25,13 +25,13 @@ check_not_expired () {
 find_commit () {
     URL=$1
     if [[ ${URL} == *"trunk"* ]]; then
-        COMMIT=`git ls-remote https://github.com/ocaml/ocaml.git refs/heads/trunk | awk -F' ' '{print $1}'`
+        COMMIT=$(git ls-remote https://github.com/ocaml/ocaml.git refs/heads/trunk | awk -F' ' '{print $1}')
     elif [[ ${URL} == *"refs/head"* || ${URL} == *"refs/pull"* ]]; then
-        GIT_SOURCE=`echo ${URL} | awk -F'/archive/' '{print $1}'`
-        REFS_PATH=`echo ${URL} | awk -F'/archive/' '{print $2}' | awk -F'.' '{print $1}'`
-        COMMIT=`git ls-remote ${GIT_SOURCE} ${REFS_PATH} | awk -F' ' '{print $1}'`
+        GIT_SOURCE=$(echo "${URL}" | awk -F'/archive/' '{print $1}')
+        REFS_PATH=$(echo "${URL}" | awk -F'/archive/' '{print $2}' | awk -F'.' '{print $1}')
+        COMMIT=$(git ls-remote "${GIT_SOURCE}" "${REFS_PATH}" | awk -F' ' '{print $1}')
     elif [[ ${URL} == *"archive"* ]]; then
-        COMMIT=`echo ${URL##*/} | awk -F'.' '{print $1}'`
+        COMMIT=$(echo "${URL##*/}" | awk -F'.' '{print $1}')
     else
         echo "Error: Unable to find commit for ${URL}"
         COMMIT=""
@@ -76,23 +76,23 @@ config_parallel () {
 # Override with raw GitHub configuration file (if provided)
 if [[ ${CUSTOM_FILE} == *"github"* ]]; then
     wget -O "${TMP_CUSTOM_FILE}" "${CUSTOM_FILE}"
-    CUSTOM_FILE=$(echo "${TMP_CUSTOM_FILE}")
+    CUSTOM_FILE="${TMP_CUSTOM_FILE}"
 fi
 
 # Number of Custom variants
-COUNT=`jq '. | length' "${CUSTOM_FILE}"`
+COUNT=$(jq '. | length' "${CUSTOM_FILE}")
 
 
 # Iterate through each variant
-for i in $(seq 0 $((${COUNT} - 1))); do
+for i in $(seq 0 $((COUNT - 1))); do
     # Iterate through each kind of benchmark
     for KIND in "sequential" "parallel" "perfstat" "pausetimes_seq" "pausetimes_par" ; do
         # Obtain configuration options
-        CONFIG_URL=`jq -r '.['$i'].url' "${CUSTOM_FILE}"`
-        CONFIG_NAME=`jq -r '.['$i'].name' "${CUSTOM_FILE}"`
+        CONFIG_URL=$(jq -r '.['"$i"'].url' "${CUSTOM_FILE}")
+        CONFIG_NAME=$(jq -r '.['"$i"'].name' "${CUSTOM_FILE}")
         CONFIG_VARIANT=$(grep -oP "^(\\d|\.)+(\\+(trunk|stable))*" <<< "${CONFIG_NAME}")
-        CONFIG_EXPIRY=`jq -r '.['$i'].expiry // empty' "${CUSTOM_FILE}"`
-        CONFIG_TAG=`jq -r '.['$i'].tag // "macro_bench"' "${CUSTOM_FILE}"`
+        CONFIG_EXPIRY=$(jq -r '.['"$i"'].expiry // empty' "${CUSTOM_FILE}")
+        CONFIG_TAG=$(jq -r '.['"$i"'].tag // "macro_bench"' "${CUSTOM_FILE}")
         CONFIG_NAME="${CONFIG_NAME}+${KIND}"
 
         # Set configuration flags according to the kind of benchmark we're
@@ -124,20 +124,20 @@ for i in $(seq 0 $((${COUNT} - 1))); do
                 ;;
         esac
 
-        CONFIG_OPTIONS=`jq -r '.['$i'].configure // empty' "${CUSTOM_FILE}"`
-        CONFIG_RUN_PARAMS=`jq -r '.['$i'].runparams // empty' "${CUSTOM_FILE}"`
-        CONFIG_ENVIRONMENT=`jq -r '.['$i'].environment // empty' "${CUSTOM_FILE}"`
-        CONFIG_OVERRIDE_PACKAGES=`jq -r '.['$i'].override_packages // empty' "${CUSTOM_FILE}"`
-        CONFIG_REMOVE_PACKAGES=`jq -r '.['$i'].remove_packages // empty' "${CUSTOM_FILE}"`
-        TAG_STRING=`echo \"${CONFIG_TAG}\"`
+        CONFIG_OPTIONS=$(jq -r '.['"$i"'].configure // empty' "${CUSTOM_FILE}")
+        CONFIG_RUN_PARAMS=$(jq -r '.['"$i"'].runparams // empty' "${CUSTOM_FILE}")
+        CONFIG_ENVIRONMENT=$(jq -r '.['"$i"'].environment // empty' "${CUSTOM_FILE}")
+        CONFIG_OVERRIDE_PACKAGES=$(jq -r '.['"$i"'].override_packages // empty' "${CUSTOM_FILE}")
+        CONFIG_REMOVE_PACKAGES=$(jq -r '.['"$i"'].remove_packages // empty' "${CUSTOM_FILE}")
+        TAG_STRING=\""${CONFIG_TAG}"\"
 
         TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-        COMMIT=$(find_commit ${CONFIG_URL})
-        NOT_EXPIRED=$(check_not_expired ${CONFIG_EXPIRY})
+        COMMIT=$(find_commit "${CONFIG_URL}")
+        NOT_EXPIRED=$(check_not_expired "${CONFIG_EXPIRY}")
 
         echo "INFO: ${TIMESTAMP} Running benchmarks for URL=${CONFIG_URL}, CONFIG_TAG=${CONFIG_TAG}, CONFIG_RUN_JSON=${CONFIG_RUN_JSON}, KIND=${KIND} for COMMIT=${COMMIT}"
 
-        if [[ ! -z "${COMMIT}" ]] && check_not_expired ${CONFIG_EXPIRY} && ! is_old_commit "${KIND}" "${HOSTNAME}" "${COMMIT}"; then
+        if [[ -n "${COMMIT}" ]] && $NOT_EXPIRED && ! is_old_commit "${KIND}" "${HOSTNAME}" "${COMMIT}"; then
             # Create results directory
             RESULTS_DIR="${SANDMARK_NIGHTLY_DIR}/sandmark-nightly/${KIND}/${HOSTNAME}/${TIMESTAMP}/${COMMIT}"
             mkdir -p "${RESULTS_DIR}"
@@ -147,19 +147,19 @@ for i in $(seq 0 $((${COUNT} - 1))); do
             cd "${TMP_DIR}"; git clone "${SANDMARK_REPO}"; cd sandmark
 
             # Prepare run JSON
-            TAG=`echo "${TAG_STRING}"` make `echo ${CONFIG_RUN_JSON}`
+            TAG="${TAG_STRING}" make "${CONFIG_RUN_JSON}"
 
             # Build and execute benchmarks
-            USE_SYS_DUNE_HACK=1 SANDMARK_URL="`echo ${CONFIG_URL}`" \
-                RUN_CONFIG_JSON="`echo ${CONFIG_RUN_JSON}`" \
-                ENVIRONMENT="`echo ${CONFIG_ENVIRONMENT}`" \
-                OCAML_CONFIG_OPTION="`echo ${CONFIG_OPTIONS}`" \
-                OCAML_RUN_PARAM="`echo ${CONFIG_RUN_PARAMS}`" \
-                SANDMARK_CUSTOM_NAME="`echo ${CONFIG_NAME}`" \
-                SANDMARK_OVERRIDE_PACKAGES="`echo ${CONFIG_OVERRIDE_PACKAGES}`" \
-                SANDMARK_REMOVE_PACKAGES="`echo ${CONFIG_REMOVE_PACKAGES}`" \
-                RUN_BENCH_TARGET="`echo ${CONFIG_RUN_BENCH_TARGET}`" \
-                BUILD_BENCH_TARGET="`echo ${CONFIG_BUILD_BENCH_TARGET}`" \
+            USE_SYS_DUNE_HACK=true SANDMARK_URL="${CONFIG_URL}" \
+                RUN_CONFIG_JSON="${CONFIG_RUN_JSON}" \
+                ENVIRONMENT="${CONFIG_ENVIRONMENT}" \
+                OCAML_CONFIG_OPTION="${CONFIG_OPTIONS}" \
+                OCAML_RUN_PARAM="${CONFIG_RUN_PARAMS}" \
+                SANDMARK_CUSTOM_NAME="${CONFIG_NAME}" \
+                SANDMARK_OVERRIDE_PACKAGES="${CONFIG_OVERRIDE_PACKAGES}" \
+                SANDMARK_REMOVE_PACKAGES="${CONFIG_REMOVE_PACKAGES}" \
+                RUN_BENCH_TARGET="${CONFIG_RUN_BENCH_TARGET}" \
+                BUILD_BENCH_TARGET="${CONFIG_BUILD_BENCH_TARGET}" \
                 make ocaml-versions/"${CONFIG_VARIANT}".bench > "${RESULTS_DIR}/${CONFIG_NAME}.${TIMESTAMP}.${COMMIT}.log" 2>&1
 
             # Copy results
--- run_all_serial.sh
+++ run_all_serial.sh
@@ -2,7 +2,7 @@
 
 TAG='"macro_bench"' make run_config_filtered.json
 
-OPT_WAIT=0 USE_SYS_DUNE_HACK=1 \
+OPT_WAIT=false USE_SYS_DUNE_HACK=true \
 	SANDMARK_CUSTOM_NAME=5.1.0+trunk \
 	RUN_CONFIG_JSON=run_config_filtered.json \
 	make ocaml-versions/5.1.0+trunk.bench
